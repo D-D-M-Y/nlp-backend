c9b5a36f5b67494f3458bfbdc8c2d8c56dcc827a
AJ4work <130941497+AJ4work@users.noreply.github.com>
2024-05-24 16:40:54 +0800

    changed model and performed bugfix
    Co-authored-by: ISAIAH LOUIS EMMANUEL S. YEE <99708491+Isaiah0706@users.noreply.github.com>

--- chatbotModule/userChatBotTraining.py
+++ chatbotModule/userChatBotTraining.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
 import nltk
-import pickle
 from string import punctuation
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize
@@ -15,7 +14,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import seaborn as sns 
 from matplotlib import pyplot as plt 
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -44,32 +43,34 @@ df = pd.DataFrame(data2)
 df.columns = ["Tag", "Input"]
 
 label_encoder.fit(df["Tag"])
-with open('pickle/label_encoder.pkl', 'wb') as f:
-  pickle.dump(label_encoder, f)
 
-df["clean_inputs"] = df["Input"].apply(lambda x: " ".join([lemmatizer.lemmatize(word.lower()) for word in x.split() if word.lower() not in [stopwords, punctuation]]))
+with open('pickle/label_encoder.pkl', 'wb') as f:
+    pickle.dump(label_encoder, f)
+
+df["clean_inputs"] = df["Input"].apply(lambda x: " ".join(lemmatizer.lemmatize(word.lower()) for word in word_tokenize(x) if word.lower() not in stopwords and word not in punctuation))
 df["tag_inputs"] = df["Tag"].apply(lambda x: label_encoder.transform([x])).apply(lambda x: x[0])
 
-textVect = TfidfVectorizer(stop_words="english", ngram_range=(1,3))
+textVect = TfidfVectorizer(stop_words="english")
 textVect.fit({word for word in df["clean_inputs"]})
 
 with open('pickle/textvect.pkl', 'wb') as f:
-  pickle.dump(textVect, f)
+    pickle.dump(textVect, f)
 
 x = df["clean_inputs"]
 y = df["tag_inputs"]
 
 x = textVect.transform(x)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
 x_train.toarray()
 
-DecisionTree = DecisionTreeClassifier()
-DecisionTree.fit(x_train, y_train)
+NaiveBayes = MultinomialNB()
+NaiveBayes.fit(x_train, y_train)
 
-with open('pickle/decisiontree.pkl', 'wb') as f:
-  pickle.dump(DecisionTree, f)
+with open('pickle/naivebayes.pkl', 'wb') as f:
+    pickle.dump(NaiveBayes, f)
 
-pred = DecisionTree.predict(x_test)
+pred = NaiveBayes.predict(x_test)
+
 print(accuracy_score(y_test, pred))
