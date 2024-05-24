cbd5303f4997d5ec348005f3d4fa0cdfe60cbae5
AJ4work <130941497+AJ4work@users.noreply.github.com>
2024-05-25 00:17:47 +0800

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from chatbotModule.userChatBotChat import gen_response, preprocess, predict, greet
 from open_ai.api_caller import interact_with_lexica
-
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config.from_pyfile('settings.py')
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 app.config.from_pyfile('settings.py')
 
@@ -10,6 +16,7 @@ def helloWorld():
     return "Hellow World"
 
 @app.route("/jude")
+@cross_origin()
 def openLexica():
     while True:
         user_input = input("Your message: ")
@@ -20,7 +27,32 @@ def openLexica():
         
         # Call the function from the imported module
         interact_with_lexica(user_input)
-        
-if __name__ == "__main__":
-    openLexica()
 
+
+
+@app.route("/openlexica/greet", methods=['GET'])
+@cross_origin()
+def greetLexica():
+    greeting = greet()
+    return jsonify({
+        "res": f"{greeting}",
+        "sender": "backend"
+    })
+
+@app.route("/openlexica/response", methods=['POST'])
+@cross_origin()
+def chatWithLexica():
+    data = request.form
+    input_chat = preprocess(data["chat"])
+    pred, check = predict(input_chat)
+    if all(prob < 0.30 for prob in check):
+        return jsonify({
+            "res": f"I don't understand your request of {data["chat"]}",
+            "sender": "backend"
+        })
+    lexica_response = gen_response(pred)
+
+    return jsonify({
+            "res": f"{lexica_response}",
+            "sender": "backend"
+        })
