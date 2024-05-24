9082f856eb329fa4bf9edcc6f05bb4f6525aa70f
AJ4work <130941497+AJ4work@users.noreply.github.com>
2024-05-24 21:31:44 +0800

    Updated chatbot training

--- chatbotModule/userChatBotTraining.py
+++ chatbotModule/userChatBotTraining.py
@@ -21,8 +21,6 @@ from matplotlib import pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('vader_lexicon')
@@ -51,10 +49,11 @@ label_encoder.fit(df["Tag"])
 with open('pickle/label_encoder.pkl', 'wb') as f:
     pickle.dump(label_encoder, f)
 
-df["clean_inputs"] = df["Input"].apply(lambda x: " ".join(lemmatizer.lemmatize(word.lower()) for word in word_tokenize(x) if word not in punctuation))
+df["clean_inputs"] = df["Input"].apply(lambda x: " ".join(lemmatizer.lemmatize(word.lower()) for word in word_tokenize(x) if word.lower not in stopwords and word not in punctuation))
 df["tag_inputs"] = df["Tag"].apply(lambda x: label_encoder.transform([x])).apply(lambda x: x[0])
 
-textVect = TfidfVectorizer(stop_words="english")
+#Tuned according to gridsearch
+textVect = TfidfVectorizer(ngram_range=(1,2))
 textVect.fit({word for word in df["clean_inputs"]})
 
 with open('pickle/textvect.pkl', 'wb') as f:
@@ -63,32 +62,19 @@ with open('pickle/textvect.pkl', 'wb') as f:
 x = df["clean_inputs"]
 y = df["tag_inputs"]
 
-# x = textVect.transform(x)
-# x = textVect.inverse_transform(x)
+x = textVect.transform(x)
+
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
-# NaiveBayes = MultinomialNB()
-# NaiveBayes.fit(x_train, y_train)
+#Tuned according to gridsearch
+NaiveBayes = MultinomialNB(fit_prior=False, alpha=0.1)
+NaiveBayes.fit(x_train, y_train)
 
-# with open('pickle/naivebayes.pkl', 'wb') as f:
-#     pickle.dump(NaiveBayes, f)
+with open('pickle/naivebayes.pkl', 'wb') as f:
+     pickle.dump(NaiveBayes, f)
 
-# pred = NaiveBayes.predict(x_test)
-
-pipeline = Pipeline(steps=[('tfidf', TfidfVectorizer()), ('mnb', MultinomialNB())])
-
-pipeline.fit(x_train, y_train)
+pred = NaiveBayes.predict(x_test)
 
 
-param_grid = {'tfidf__ngram_range': [(1,1), (1,2), (1,3)],
-              'tfidf__stop_words': [None, 'english'],
-              'tfidf__use_idf': [True, False],
-              'mnb__fit_prior': [True, False],
-              'mnb__alpha': [1.0, 0.5, 0.0, 0.1, 1e-2, 1e-3]}
-
-grid = GridSearchCV(pipeline, param_grid, scoring='accuracy', cv=2, n_jobs=2, verbose=1)
-grid.fit(x_train,y_train)
-
-print('Best score', grid.best_score_)
-print('Best params', grid.best_params_)
+print(accuracy_score(y_test, pred))
